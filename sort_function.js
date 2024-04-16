5699dcc0406d644306f2d063df302b3a0dc74e6e
virajvd <33964134+virajvd@users.noreply.github.com>
2024-04-16 18:16:43 +0530

    Update sort_function.js
    Added insertion_sort function

--- sort_function.js
+++ sort_function.js
@@ -2,7 +2,11 @@ let print = require('./printFunction')
 let exp = require('./constant.json')
 
 module.exports.bubblesort = function bubblesort(arr, view = false){
-    if(view) print.explainPrint(exp?.sort_explaination?.bubblesort)
+    if(view) {
+        print.explainPrint(exp?.sort_explaination?.bubblesort)
+        print.internalPrint2(arr, "Initial Array")
+        console.log('----------------------------------------')
+    }
     let swapped = false
     for(let i=0;i<arr.length;i++){
         swapped = false
@@ -19,6 +23,53 @@ module.exports.bubblesort = function bubblesort(arr, view = false){
             }
         }
         if (swapped===false) break
+    }
+    if(view){
+        print.finalPrint(arr, "Sorted array is: ")
+    }
+    return arr
+}
+
+module.exports.insertionsort = function insertionsort(arr, view = false){
+    if(view) {
+        print.explainPrint(exp?.sort_explaination?.insertionsort)
+        print.internalPrint2(arr, "Initial Array")
+        console.log('----------------------------------------')
+    }
+    for(let i=1;i<arr.length;i++){
+        let key = arr[i]  
+        if(view) print.intermediatePrint(`${key} at index ${i} picked to find its correct position`)     
+        let j = i-1        
+        while(j>=0 && arr[j]>key){
+            arr[j+1] = arr[j]
+            j = j-1            
+        }
+        arr[j+1] = key
+        if(view && i==j+1){
+            print.internalPrint2(arr, `${key} already at its correct position`)
+        }
+        else if (view && i!=j+1){
+            print.internalPrint2(arr, `${key} at index ${i} moved to index ${(j+1)}`)
+            let k=0            
+            while(k<(j+1)){
+                process.stdout.write('  ')
+                k++
+            }
+            process.stdout.write(' ^')            
+            while(k<i){
+                if(k==i-1) process.stdout.write("=>")
+                else process.stdout.write("==")
+                k++
+            }
+            console.log()
+            k=0
+            while(k<(j+1)){
+                process.stdout.write('  ')
+                k++
+            }
+            process.stdout.write(' |\n')            
+        }       
+        console.log()
     }
     if(view){
         print.finalPrint(arr, "Sorted array is: ")
